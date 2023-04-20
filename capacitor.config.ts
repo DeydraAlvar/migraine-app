395f669885afee8388b6bdfb9b4d2703126fe9dd
Deydra Alvarado Acevedo <deydra.alvaradoacevedo@pfizer.com>
2023-04-19 18:38:11 -0600

    second commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'pfizer-migraine',
+  appName: 'Migraine Assist',
   webDir: 'www',
   bundledWebRuntime: false
 };
